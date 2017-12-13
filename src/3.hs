f2c668ebc935f4dc48026f592716f01e6576e5ce
Ilya Pikulin <ilya.pikulin@gmail.com>
2017-12-13 12:33:12 +0300

    Rewrite in a somewhat more clear way

--- src/3.hs
+++ src/3.hs
@@ -1,21 +1,9 @@
 import Data.List
+import Data.Maybe
 
-targetNumber = 600851475143
-
--- sieveOfEratosthenes :: Integer -> [Integer]
--- sieveOfEratosthenes bound = f [2,3..bound]
---   where
---     f :: [Integer] -> [Integer]
---     f (prime:rest) = [prime] ++ (f $ filter (\num -> num `mod` prime /= 0) rest)
---     f [] = []
-
-isPrime n = case find (\x -> n `mod` x == 0) $ [2] ++ [3, 5..bound] of 
-              Nothing -> True 
-              otherwise -> False
-  where
-    bound = floor $ sqrt $ fromInteger n :: Integer
-    
 main :: IO ()
-main = print $ last $ filter (\x -> targetNumber `mod` x == 0 && isPrime x) $ [2] ++ [3, 5..bound]
-  where
-    bound = floor $ sqrt $ fromInteger targetNumber :: Integer
+main = print $ (last . filter (\x -> targetNumber `mod` x == 0 && isPrime x)) (2 : [3, 5..bound])
+  where bound = (floor . sqrt . fromInteger) targetNumber :: Integer
+        targetNumber = 600851475143
+        isPrime n = (isJust . find (\x -> n `mod` x == 0)) (2 : [3, 5..bound])
+          where bound = (floor . sqrt . fromInteger) n :: Integer
