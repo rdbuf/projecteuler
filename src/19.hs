f6e10d2f9f3210b6d2a3ad99e43e1d8ff9d5a432
Ilya Pikulin <ilya.pikulin@gmail.com>
2018-03-30 01:36:32 +0300

    Solve problem #19

---
+++ src/19.hs
@@ -0,0 +1,14 @@
+-- projecteuler 19
+
+import Foreign.Marshal.Utils
+import Control.Monad
+import Data.Monoid
+
+isLeap n = (n `mod` 4 == 0) && (n `mod` 100 /= 0 || n `mod` 400 == 0)
+
+daysInMonth year = [31, 28 + fromBool (isLeap year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+data Day = Day { dayOfWeek :: Int, dayOfMonth :: Int }
+
+main = print . length . filter valid . zipWith Day ([1 + sum (daysInMonth 1900) `mod` 7..7] <> (join . repeat) [1..7]) $ enumFromTo 1 =<< daysInMonth =<< [1901..2000] where
+    valid (Day dow dom) = (dow == 7) && (dom == 1)
